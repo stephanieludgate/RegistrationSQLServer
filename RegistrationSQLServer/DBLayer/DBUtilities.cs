9e1a295c81639c26134d3866be028f20863caabe
Steph <stephanieludgate@gmail.com>
2020-05-20 13:24:16 -0400

    Created initial for Registration, added 2 projects

---
+++ RegistrationSQLServer/DBLayer/DBUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RegistrationSQLServer.DBLayer
+{
+    public class DBUtilities
+    {
+        private static SqlConnection GetSqlConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ToString();
+            return new SqlConnection(connectionString);
+        }
+
+        public static int InsertUserInformation(BusinessLayer.UserInformation userInfo)
+        {
+            int result;
+
+            using (SqlConnection cnn = GetSqlConnection())
+            {
+                String sql = $"INSERT INTO UserInformation(FirstName, LastName, Address, City, Province, PostalCode, Country) VALUES ('{userInfo.FirstName}', '{userInfo.LastName}', '{userInfo.Address}', '{userInfo.City}', '{userInfo.Province}', '{userInfo.PostalCode}', '{userInfo.Country}')";
+                using (SqlCommand command = new SqlCommand(sql, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        cnn.Open();
+                        adapter.InsertCommand = new SqlCommand(sql, cnn);
+                        result = adapter.InsertCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ RegistrationSQLServer/Registration.aspx.cs
@@ -0,0 +1,47 @@
+﻿using RegistrationSQLServer.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RegistrationSQLServer
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void enterInfoButton_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid)
+            {
+                // send user info to database
+                BusinessLayer.UserInformation ui = new BusinessLayer.UserInformation();
+
+
+                ui.FirstName = Server.HtmlEncode(firstNameTextBox.Text);
+                ui.LastName = Server.HtmlEncode(lastNameTextBox.Text);
+                ui.Address = Server.HtmlEncode(addressTextBox.Text);
+                ui.City = Server.HtmlEncode(cityTextBox.Text);
+                ui.Province = Server.HtmlEncode(stateOrProvinceTextBox.Text);
+                ui.PostalCode = Server.HtmlEncode(zipCodeTextBox.Text);
+                ui.Country = Server.HtmlEncode(countryTextBox.Text);
+
+                if(DBLayer.DBUtilities.InsertUserInformation(ui) == 1)
+                {
+                    lblResult.Text = "User was successfully added!";
+                }
+                else
+                {
+                    lblResult.Text = "Uh oh!";
+                }
+                
+                //Response.Redirect("RegistrationResult.aspx");
+            }
+        }
+    }
+}
